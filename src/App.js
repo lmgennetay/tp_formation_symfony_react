fbc76c5b915e2df706883a64cd883e136f4d6f69
Louis-Marie GENNETAY <louismarie.gennetay@viacesi.fr>
2019-07-12 14:37:20 +0200

    Menu

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 class App extends Component {
-  state = {
-    apprenants: []
+  constructor() {
+    super();
+    this.state = {
+      apprenants: []
+    };
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li>
+              <Link to="/apprenant">Home</Link>
+            </li>
+            <li>
+              <Link to="/formateur">About</Link>
+            </li>
+            <li>
+              <Link to="/module">Topics</Link>
+            </li>
+          </ul>
+
+          <hr />
+          <div className="main-route-place"> 
+            <Route exact path="/" component={Apprenant} />
+            <Route path="/apprenant" component={Apprenant} />
+            <Route path="/formateur" component={Formateur} />
+            <Route path="/module" component={Module} />
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+class Apprenant extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      apprenants: []
+    };
   }
   componentDidMount() {
     fetch('https://localhost:8000/apprenant/liste_apprenants')
@@ -22,7 +62,7 @@ class App extends Component {
             <div className="card">
               <div className="card-body">
                 <h5 className="card-title">{apprenant.prenom} {apprenant.nom}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">{apprenant.dateNaissance.date.toLocaleDateString()}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">{apprenant.dateNaissance.date}</h6>
                 <h6 className="card-subtitle mb-2 text-muted">{apprenant.tel}</h6>
                 <h6 className="card-subtitle mb-2 text-muted">{apprenant.email}</h6>
               </div>
