84823c905e0125211f2a870af2666e94124b8827
Louis-Marie GENNETAY <louismarie.gennetay@viacesi.fr>
2019-07-12 13:53:10 +0200

    Chargement de l'API Apprenant

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    apprenants: []
+  }
+  componentDidMount() {
+    fetch('https://localhost:8000/apprenant/liste_apprenants')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ apprenants: data.apprenants })
+      console.log(this.state.apprenants)
+    })
+    .catch(console.log)
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="col-xs-12">
+          <h1>Apprenants</h1>
+          {this.state.apprenants.map((apprenant) => (
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{apprenant.prenom} {apprenant.nom}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{apprenant.dateNaissance.date.toLocaleDateString()}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">{apprenant.tel}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">{apprenant.email}</h6>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
